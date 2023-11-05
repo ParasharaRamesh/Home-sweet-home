1403213b16c2ddfa3843266712fdb7e3a059a9da
Parashara <parasharar@gmail.com>
2023-11-05 17:16:46 +0800

    tried haversine as well

--- experiments/KNN/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-    ext_modules = cythonize("custom_distance_metric.pyx"),
-    include_dirs = [np.get_include()]
-)
